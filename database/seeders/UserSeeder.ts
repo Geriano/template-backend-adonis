e0bfa7d85234a22f7ea1a2833fe8771452d1a3ce
Geriano <gerznewbie@gmail.com>
2022-09-10 17:17:33 +0700

    adding superuser role

--- database/seeders/UserSeeder.ts
+++ database/seeders/UserSeeder.ts
@@ -1,14 +1,19 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Role from 'App/Models/Superuser/Role'
 import User from 'App/Models/User'
 
 export default class extends BaseSeeder {
   public async run () {
     // Write your database queries inside the run method
-    await User.create({
+    const user = await User.create({
       name: 'superuser',
       username: 'su',
       email: 'su@local.app',
       password: 'password',
     })
+
+    await user.related('roles').attach([
+      (await Role.firstOrFail()).id
+    ])
   }
 }
