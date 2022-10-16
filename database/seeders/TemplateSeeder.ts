b4fe47e55fdf86e031db97c79dd7dfeaa593bdd1
Geriano <gerznewbie@gmail.com>
2022-10-16 13:26:11 +0700

    now initial seeder just in one file

--- database/seeders/TemplateSeeder.ts
+++ database/seeders/TemplateSeeder.ts
@@ -1,10 +1,45 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Menu from 'App/Models/Superuser/Menu'
 import Permission from 'App/Models/Superuser/Permission'
+import Role from 'App/Models/Superuser/Role'
+import User from 'App/Models/User'
 
 export default class extends BaseSeeder {
+  public permissions = [
+    'permission',
+    'role',
+    'user',
+    'menu',
+  ]
+
   public async run () {
-    // Write your database queries inside the run method
+    await Role.create({
+      name: 'superuser',
+    })
+    
+    for (let permission of this.permissions) {
+      for (let ability of ['create', 'read', 'update', 'delete']) {
+        await Permission.create({
+          name: `${ability} ${permission}`
+        })
+      }
+    }
+    
+    const user = await User.create({
+      name: 'superuser',
+      username: 'su',
+      email: 'su@local.app',
+      password: 'password',
+    })
+
+    await user.related('roles').attach([
+      (await Role.firstOrFail()).id
+    ])
+
+    await this.menus()
+  }
+
+  private async menus () {
     const dashboard = await Menu.create({
       name: 'dashboard',
       icon: 'mdi mdi-view-dashboard',
